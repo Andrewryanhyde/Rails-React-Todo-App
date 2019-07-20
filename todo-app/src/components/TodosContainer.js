85be43f33906bcb81b891d747ad28b5bbe8a505e
Andrewryanhyde <andrewryanhyde@hotmail.co.uk>
2019-07-20 15:46:39 +0100

    Set TodosContainer to initialise state and added ComponentDidMount function to load list of tasks

--- todo-app/src/components/TodosContainer.js
+++ todo-app/src/components/TodosContainer.js
@@ -1,13 +1,12 @@
-import React, { Component } from 'react';
-import axios from 'axios';
+import React, { Component } from 'react'
+import axios from 'axios'
 
 class TodosContainer extends Component {
   constructor(props) {
-    super(props) 
-      this.state = {
-        todos: [],
-        inputValue: ''
-      }
+    super(props)
+    this.state = {
+      todos: []
+    }
   }
 
   getTodos() {
@@ -19,19 +18,31 @@ class TodosContainer extends Component {
   }
 
   componentDidMount() {
-    this.getTodos();
+    this.getTodos()
   }
 
   render() {
-    return(
+    return (
       <div>
         <div className="inputContainer">
-          <input className="taskInput" type="text" placeholder="Add a task" maxLength="50" />
-        </div>
-        <div className="listWrapper">
-          <ul className="taskList"></ul>
-        </div>
-      </div>
+          <input className="taskInput" type="text" 
+            placeholder="Add a task" maxLength="50"
+            onKeyPress={this.createTodo} />
+        </div>  	    
+	<div className="listWrapper">
+	   <ul className="taskList">
+		  {this.state.todos.map((todo) => {
+		    return(
+		      <li className="task" todo={todo} key={todo.id}>
+			<input className="taskCheckbox" type="checkbox" />              
+			<label className="taskLabel">{todo.title}</label>
+			<span className="deleteTaskBtn">x</span>
+		      </li>
+		    )       
+		  })} 	    
+	   </ul>
+	</div>
+     </div>
     )
   }
 }
